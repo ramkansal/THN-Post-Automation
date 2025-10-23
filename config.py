87e72f91fec581ce2307933431c5d143b8491951
Ram001-code <ramkansal9822@gmail.com>
2025-10-23 21:09:07 +0530

    Added Hashtags

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from zoneinfo import ZoneInfo
 DEFAULT_FEED = "https://feeds.feedburner.com/TheHackersNews?format=xml"
 IST = ZoneInfo("Asia/Kolkata")
 UA = "thn-post-kit/2.0 (+https://thehackernews.com)"
-HASHTAGS = "#cybersecurity #infosec #TheHackerNews"
+HASHTAGS = "#CyberSecurity #InfoSec #Hacking #DataBreach #CyberAttack #ThreatIntel #SecurityNews #CyberThreat #Ransomware"
 APP_TITLE = "THN Post Kit"
 DEFAULT_OUT = os.environ.get("THN_OUT_DIR", "./THN")
 FLASK_SECRET = os.environ.get("FLASK_SECRET")
