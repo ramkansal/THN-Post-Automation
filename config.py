bc03a8883b36e680001b1e69399da6d11d5ab0b2
Ram001-code <ramkansal9822@gmail.com>
2025-10-24 18:22:11 +0530

    RATE_LIMIT_FEED_CALLS, RATE_LIMIT_WINDOW_SEC, ALLOW_ONLY_DEFAULT_FEED

--- config.py
+++ config.py
@@ -9,3 +9,8 @@ APP_TITLE = "THN Post Kit"
 DEFAULT_OUT = os.environ.get("THN_OUT_DIR", "./THN")
 FLASK_SECRET = os.environ.get("FLASK_SECRET")
 PORT = int(os.environ.get("PORT", 5000))
+
+RATE_LIMIT_FEED_CALLS = int(os.environ.get("RATE_LIMIT_FEED_CALLS", 10))
+RATE_LIMIT_WINDOW_SEC = int(os.environ.get("RATE_LIMIT_WINDOW_SEC", 60))
+
+ALLOW_ONLY_DEFAULT_FEED = os.environ.get("ALLOW_ONLY_DEFAULT_FEED", "1") not in ("0", "false", "False")
